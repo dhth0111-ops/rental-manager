54dcdc26162fd1e7f941dcda3ef921049bbb81e9
dhth0111-ops <dhth0111@gmail.com>
2026-08-08 18:35:46 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,35 +1,37 @@
-const CACHE='rental-manager-v1.4.1';
+const CACHE='rental-manager-v1.6-complete';
 const ASSETS=[
   './',
   './index.html',
   './manifest.json',
+  './building-hero.jpg',
   './icon-192.png',
   './icon-512.png',
   './icon-maskable-192.png',
   './icon-maskable-512.png',
-  './apple-touch-icon.png',
-  './building-hero.jpg'
+  './apple-touch-icon.png'
 ];
 
-self.addEventListener('install', event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event=>{
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', event=>{
-  if(event.request.method!=='GET') return;
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
   event.respondWith(
-    fetch(event.request).then(res=>{
-      const copy=res.clone();
-      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-      return res;
-    }).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html')))
+    fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match(event.request).then(r => r || caches.match('./index.html')))
   );
 });
